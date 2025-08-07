d33d8e34293bdf324bdb237a985b975fc684b1da
Carlos Danny Campoverde Encarnación <cdcampoverde@espe.edu.ec>
2025-08-07 08:24:22 -0500

    Cambio en el index2.js

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Servidor ejecutandose en  el puerto ${PORT}`);
+    console.log(`El servidor ejecutandose en  el puerto ${PORT}`);
 });
